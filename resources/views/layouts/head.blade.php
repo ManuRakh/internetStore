6313b44d49541009c497d80198fb787ebb522dce
ManuRakh <manucher5160@gmail.com>
2018-02-16 21:14:37 +0300

    First Commits

---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+
+<html>
+<head>
+<title>Gretong a Ecommerce Category Flat Bootstarp Responsive Website Template | Home :: w3layouts</title>
+<link href="{{asset('/public/css/bootstrap.css')}}" rel='stylesheet' type='text/css' />
+<!-- jQuery (necessary JavaScript plugins) -->
+<script type='text/javascript' src="{{asset('/public/js/jquery-1.11.1.min.js')}}"></script>
+<!-- Custom Theme files -->
+<link href="{{asset('/public/css/style.css')}}" rel='stylesheet' type='text/css' />
+<!-- Custom Theme files -->
+<!--//theme-style-->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Gretong Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href='http://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Playfair+Display:400,700,900' rel='stylesheet' type='text/css'>
+<!-- start menu -->
+<link href="{{asset('/public/css/megamenu.css')}}" rel="stylesheet" type="text/css" media="all" />
+<script type="text/javascript" src="{{asset('/public/js/megamenu.js')}}"></script>
+<script>$(document).ready(function(){$(".megamenu").megamenu();});</script>
+<script src="{{asset('/public/js/menu_jquery.js')}}"></script>
+<script src="{{asset('/public/js/simpleCart.min.js')}}"> </script>
+</head>
