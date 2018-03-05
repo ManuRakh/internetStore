1bdf7bd68694e96e5e54227f734e6fe823a1f9e9
ManuRakh <manucher5160@gmail.com>
2018-03-06 00:19:42 +0300

    footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,11 +2,11 @@
 <div class="footer">
 	<div class="container">
 		<div class="col-md-3 cust">
-			<h4>CUSTOMER CARE</h4>
-				<li><a href="#">Help Center</a></li>
-				<li><a href="#">FAQ</a></li>
-				<li><a href="#">How To Buy</a></li>
-				<li><a href="#">Delivery</a></li>
+			<!--<h4>CUSTOMER CARE</h4>-->
+			<!--	<li><a href="#">Help Center</a></li>-->
+			<!--	<li><a href="#">FAQ</a></li>-->
+			<!--	<li><a href="#">How To Buy</a></li>-->
+				<li><a href="{{route('checkout')}}">Delivery</a></li>
 		</div>
 		<div class="col-md-2 abt">
 			<h4>ABOUT US</h4>
@@ -18,9 +18,8 @@
 		<div class="col-md-2 myac">
 			<h4>MY ACCOUNT</h4>
 				<li><a href="{{route('register')}}">Register</a></li>
-				<li><a href="#">My Cart</a></li>
-				<li><a href="#">Order History</a></li>
-				<li><a href="#">Payment</a></li>
+				<li><a href="{{route('checkout')}}">My Cart</a></li>
+				<!--<li><a href="#">Payment</a></li>-->
 		</div>
 		<div class="col-md-5 our-st">
 			<div class="our-left">
@@ -37,9 +36,9 @@
 				</div>
 			</div>
 			<div class="clearfix"> </div>
-				<li><i class="add"> </i>Jl. Haji Muhidin, Blok G no.69</li>
-				<li><i class="phone"> </i>025-2839341</li>
-				<li><a href="#"><i class="mail"> </i>info@sitename.com </a></li>
+				<li><i class="add"> </i>North Cyprus . CIU Bufavento 265</li>
+				<li><i class="phone"> </i>+905488675590</li>
+				<li><a href="#"><i class="mail"> </i>manucher5160@gmail.com </a></li>
 			
 		</div>
 		<div class="clearfix"> </div>
